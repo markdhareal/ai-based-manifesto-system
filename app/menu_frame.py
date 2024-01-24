3fd207d92e8fc2cf70e6c9cd11e56063aa163d53
Dharyl <programmingdha@gmail.com>
2024-01-25 00:38:24 +0800

    created menu with buttons

--- app/menu_frame.py
+++ app/menu_frame.py
@@ -1,7 +1,22 @@
 # MENU FOR MAIN FRAME
 import customtkinter as ctk
+from utils.functional_widgets import FunctionalWidgets
 
 class MenuFrame(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent)
         self.place(x = 0, y = 0, relwidth = 0.2, relheight = 1)
+
+        self.create_widgets()
+
+    def create_widgets(self):
+
+        # CREATE WIDGETS
+        manage_boat_button = FunctionalWidgets.create_button(self, "Manage Boat", height=40)
+        manage_passengers_button = FunctionalWidgets.create_button(self, "Manage Passengers", height=45)
+        enlist_button = FunctionalWidgets.create_button(self, "Enlist Passenger", height=45)
+
+        #CREATE LAYOUT
+        manage_boat_button.pack(side="top", expand=True)
+        manage_passengers_button.pack(side="top", expand=True)
+        enlist_button.pack(side="top", expand=True)
--- utils/functional_widgets.py
+++ utils/functional_widgets.py
@@ -4,7 +4,5 @@ import customtkinter as ctk
 class FunctionalWidgets(ctk.CTk):
 
     def create_button(window, text, **kwargs):
-
         button = ctk.CTkButton(window, text=text, **kwargs)
-
         return button
