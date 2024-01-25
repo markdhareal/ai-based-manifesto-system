ff8a3bff5c981d4c165ad28c76898f6ed8ea9f5a
Dharyl <programmingdha@gmail.com>
2024-01-25 17:17:09 +0800

    added images folder and sample images for buttons

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from app.main_frame import App
 import customtkinter as ctk
 
 ctk.set_appearance_mode("System")
-ctk.set_default_color_theme("dark-blue")
+ctk.set_default_color_theme("blue")
 
 
 if __name__ == "__main__":
