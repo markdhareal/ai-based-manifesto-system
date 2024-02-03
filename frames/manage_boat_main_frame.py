923504f21a2f73ddfa07a2127855112d2086f807
Dharyl <programmingdha@gmail.com>
2024-02-03 18:34:03 +0800

    Already Implement the Class Based TreeView

--- frames/manage_boat_main_frame.py
+++ frames/manage_boat_main_frame.py
@@ -5,7 +5,23 @@ class ManageBoatMainFrame(ctk.CTkFrame):
     def __init__(self,parent):
         super().__init__(parent)
 
-        # ctk.CTkLabel(self,fg_color="red").pack(fill="both", expand=True)
+        # ctk.CTkLabel(self,fg_color="red", height=20).pack()
         self.place(relx = 0.3, y = 0, relwidth = 0.7, relheight=1)
+        self.create_tree()
 
+        # self.tree = MyTreeView(self)
+
+    def create_tree(self):
         self.tree = MyTreeView(self)
+        
+        info = [
+            ("Boat Owner"),
+            ("Boat Name"),
+            ("Boat Captain"),
+            ("Boat Capacity"),
+            ("Boat Crew")
+        ]
+
+        self.tree.create_tree_view(info)
+
+        self.tree.pack(fill="both", expand=True)
--- utils/tree_view.py
+++ utils/tree_view.py
@@ -7,27 +7,51 @@ class MyTreeView(ctk.CTkFrame):
     def __init__(self,parent):
         super().__init__(parent)
 
-        self.pack(fill="both", expand=True)
+        self.my_tree = ttk.Treeview(self)
 
-        self.create_tree_view()
-
+    def create_tree_view(self, tree_info):
     
-    def create_tree_view(self):
-        my_tree = ttk.Treeview(self)
-
         style = ttk.Style()
         style.theme_use('clam')
         style.configure("Treeview", font=("Arial Bold", 15))
 
-        my_tree['columns'] = ("Boat Owner", "Boat Name", "Boat Captain")
+        self.my_tree['columns'] = tuple(tree_info)
 
-        my_tree.column('#0', width=0, stretch=tk.NO)
-        my_tree.column('Boat Owner', anchor=tk.CENTER, width=120)
-        my_tree.column('Boat Name', anchor=tk.CENTER, width=120)
-        my_tree.column('Boat Captain', anchor=tk.CENTER, width=120)
+        self.my_tree.column('#0', width=0, stretch=tk.NO)
 
-        my_tree.heading('Boat Owner', text='Boat Owner')
-        my_tree.heading('Boat Name', text='Boat Name')
-        my_tree.heading('Boat Captain', text='Boat Captain')
+        for i, info_heading_and_column in enumerate(tree_info):
 
-        my_tree.pack(fill="both", expand=True)
+            column = tree_info[i]
+
+            self.my_tree.column(column, anchor=tk.CENTER, width=120)
+
+            self.my_tree.heading(column, text=info_heading_and_column)
+
+        self.my_tree.pack(fill="both", expand=True)
+
+
+
+    #     self.pack(fill="both", expand=True)
+
+    #     self.create_tree_view()
+
+    
+    # def create_tree_view(self):
+    #     my_tree = ttk.Treeview(self)
+
+    #     style = ttk.Style()
+    #     style.theme_use('clam')
+    #     style.configure("Treeview", font=("Arial Bold", 15))
+
+    #     my_tree['columns'] = ("Boat Owner", "Boat Name", "Boat Captain")
+
+    #     my_tree.column('#0', width=0, stretch=tk.NO)
+    #     my_tree.column('Boat Owner', anchor=tk.CENTER, width=120)
+    #     my_tree.column('Boat Name', anchor=tk.CENTER, width=120)
+    #     my_tree.column('Boat Captain', anchor=tk.CENTER, width=120)
+
+    #     my_tree.heading('Boat Owner', text='Boat Owner')
+    #     my_tree.heading('Boat Name', text='Boat Name')
+    #     my_tree.heading('Boat Captain', text='Boat Captain')
+
+    #     my_tree.pack(fill="both", expand=True)
